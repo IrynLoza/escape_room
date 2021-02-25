86ad576d8d7ea3d1b3068a8d0bf861f8a4d0be34
Iryna Loza <irynloza@gmail.com>
2021-02-24 19:02:55 -0800

    Update keypad and light

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+function onOffLight() {
+    document.getElementById('mirror').style.borderColor = 'orchid'
+    document.getElementById('picture').style.borderColor = 'orchid'
+    document.getElementById('keypad').style.borderColor = 'orchid'
+    document.getElementById('light-top').style.borderBottomColor = 'orchid'
+    document.getElementById('wall-note').style.color = 'orchid'
+    console.log('I am clicked')
+}
